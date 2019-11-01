d9ff42b21fe35e04689fc747d0596dd97200e9a7
ZxxWs <zxx748361145@outlook.com>
2019-11-01 17:35:40 +0800

    2

---
+++ 菜鸟教程/11.线性代数计算.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+'''
+NumPy 提供了线性代数函数库 linalg，该库包含了线性代数所需的所有功能，可以看看下面的说明：
+
+函数					描述
+dot						两个数组的点积，即元素对应相乘。
+vdot					两个向量的点积
+inner					两个数组的内积
+matmul					两个数组的矩阵积
+determinant				数组的行列式
+solve					求解线性矩阵方程
+inv						计算矩阵的乘法逆矩阵
+
+'''
